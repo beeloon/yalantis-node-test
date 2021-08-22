683c40fb07d7a1180351096c0d6454f026cca98d
Bohdan Bilyk <bohdanbilykwork@gmail.com>
2021-08-22 16:46:38 +0300

    feat: add request validation to app

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
-  age: number;
-  breed: string;
+
+  @IsNotEmpty()
+  surname: string;
+
+  @IsEmail()
+  email: string;
 }
